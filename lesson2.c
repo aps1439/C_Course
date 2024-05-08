7fb02ca72e6a8a42e48bb8a9670b1e22e719002d
aps1439 <aps1439@hotmail.com>
2024-05-08 23:55:02 +0200

    Add lesson3.c Exercise about arrays

--- lesson2.c
+++ lesson2.c
@@ -4,4 +4,5 @@ int main()  {
     int a = 1, b = 2, c = 3, d;
     d = a + b + c;
     printf("%d" , d);
+    return 0;
 }
---
+++ lesson3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int cavalos[5];
+int media;
+cavalos[0] = 1;
+cavalos[1] = 2;
+cavalos[2] = 3;
+cavalos[3] = 4;
+cavalos[4] = 5;
+media = (cavalos[0] + cavalos[1] + cavalos[2] + cavalos[3] + cavalos[4]) / 5;
+printf("a media e: %d", media);
+return 0;
+}
+
+ 
